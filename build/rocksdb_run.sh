3bcd2e6ef4509a2e7c0ee01c4690b56357d246b2
Ubuntu <azureuser@NoSQLTest.maot013cew3uxdf3fyuecnmrad.bx.internal.cloudapp.net>
2022-04-21 21:56:47 +0000

    [Test] Update RocksDB tests.

--- build/rocksdb_run.sh
+++ build/rocksdb_run.sh
@@ -4,4 +4,4 @@ ycsb_root=$git_root/lib/YCSB
 
 sudo $ycsb_root/bin/ycsb load rocksdb -s -P $workload -p recordcount=$count -p rocksdb.dir=/tmp/ycsb-rocksdb-data -threads 10 | sudo tee $out_path/outputLoad.txt
 
-sudo $ycsb_root/bin/ycsb run rocksdb -s -P $workload -p recordcount=$count -p rocksdb.dir=/tmp/ycsb-rocksdb-data -p operationcount=$(($count)) -threads 10 | sudo tee $out_path/outputLoad.txt
+sudo $ycsb_root/bin/ycsb run rocksdb -s -P $workload -p recordcount=$count -p rocksdb.dir=/tmp/ycsb-rocksdb-data -p operationcount=$(($count)) -threads 10 | sudo tee $out_path/outputRun.txt
